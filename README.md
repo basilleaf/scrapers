5bf1ad2c6bac29876df4e2a3eb85ea208990daca
lisa ballard <lballard.cat@gmail.com>
2012-11-18 21:10:41 -0800

    rockets now scrapes all detail pages and added a readme

---
+++ README.md
@@ -0,0 +1,5 @@
+<p><a href = "http://grin.hq.nasa.gov/">NASA GRIN</a> is an image library, this is a scrape of the <a href = "http://grin.hq.nasa.gov/BROWSE/rocket_launch.html"
+>Rocket Launch section</a>, around 75 notable launch images with full content and links to various sizes.
+</p>
+
+grab the data at ScraperWiki: https://scraperwiki.com/scrapers/nasa_grin_rocket_launch_images/
--- rockets/scrape.py
+++ rockets/scrape.py
@@ -1,12 +1,22 @@
+import scraperwiki
 import os.path, urllib  
 import exceptions, urllib2, re
 from BeautifulSoup import BeautifulStoneSoup
 import sys
+import re
+import collections
 
-# url = 'http://grin.hq.nasa.gov/BROWSE/rocket_launch.html' # all the things
-url = 'http://grin.hq.nasa.gov/BROWSE/rocket_launch_7.html' # just one page
+base_url = 'http://grin.hq.nasa.gov' # base url of the website
+url = 'http://grin.hq.nasa.gov/BROWSE/rocket_launch.html' # index page with all the things
+# url = 'http://grin.hq.nasa.gov/BROWSE/rocket_launch_7.html' # just one page of index
 
-base_url = 'http://grin.hq.nasa.gov'
+# fields and patterns to search for in the detail pages (GRIN has it's own special tags around data)
+all_patterns = {
+	'GRIN_id' : '<!-- GRINNUMBER-BEGIN -->(.*?)<!-- GRINNUMBER-END -->',
+	'title' : '<!-- ONE-LINE-DESCRIPTION-BEGIN -->(.*?)<!-- ONE-LINE-DESCRIPTION-END -->',
+	'description' : '<!-- DESCRIPTION-BEGIN -->(.*?)<!-- DESCRIPTION-END -->',
+	'keywords' : '<!-- KEYWORD-BEGIN -->(.*?)<!-- KEYWORD-END -->',
+}
 
 try:
     page = urllib2.urlopen(url).read()   
@@ -17,26 +27,35 @@ soup = BeautifulStoneSoup(page)
 all_links = soup.findAll('a')   
 
 
-
-
-
-images = {}
+# get all the detail pages:
+all_pages = []
 for link in all_links:
-	if len(images) == 0:
-		# first look for the image link
-		if link.find('img'):
-			images['title'] = str(link.find('img')['alt'])
-			images['large_image'] = base_url + link['href']
-			images['thumbnail'] = base_url + str(link.find('img')['src'])
+	if str(link.contents[0]) == 'More information':
+		all_pages.append(base_url + link['href'])
 
-	if len(images) == 3:
-		if str(link.contents[0]) == 'More information':
-			# we have the image link, look for the more_info link
-			images['more_info_link'] = base_url + link['href']
-			print 'image array:'
-			print images
-			images = {}
-			print '------------------------------'
+# scrape data from each detail page
+for page_url in all_pages:
+	try:
+	    page = urllib2.urlopen(page_url).read()   
+	except urllib2.HTTPError:
+	    raise Exception("Http 404 " + page_url)
+	
+	try:
+		data = collections.OrderedDict() # but ScraperWiki not liking..
+		soup = BeautifulStoneSoup(page) 
+		data['credit'] = ''.join(str(soup.findAll('ul')[1]).splitlines())
+		for field, pattern in all_patterns.items():
+			data[field] = ('').join(re.findall(pattern,page,re.DOTALL|re.MULTILINE)[0].splitlines())
 
-# scraperwiki.sqlite.save(unique_keys=['Time'], data=data)
+		data['Thumbnail'] = soup.find('a',text='Thumbnail').parent['href']
+		data['Small'] = soup.find('a',text='Small').parent['href']
+		data['Medium'] = soup.find('a',text='Medium').parent['href']
+		data['Large'] = soup.find('a',text='Large').parent['href']
+
+		# update scraperwiki
+		scraperwiki.sqlite.save(unique_keys=['GRIN_id'], data=data)
+
+	except:
+		pass
+
 
